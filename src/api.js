85418acd1254b356be8d3d88826e19c8f7dff046
Cristian Caroli <cristiancaroli@gmail.com>
2015-05-11 00:56:36 +0200

    Jade integration and some refactoring.

---
+++ src/api.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var Cards = require('./cards');
+
+var questions = Cards.questions;
+var answers = Cards.answers;
+
+const nquestions = questions.length, nanswers = answers.length;
+const question = () => { return questions[Math.floor(Math.random()*nquestions)] };
+const answer = () => { return answers[Math.floor(Math.random()*nanswers)] };
+const pick = () => { return { question: question(), answer: answer() } };
+
+router.get('/question', (req, res) => res.json(question()));
+router.get('/questions', (req, res) => res.json(questions));
+
+router.get('/answer', (req, res) => res.json(answer()));
+router.get('/answers', (req, res) => res.json(answers));
+
+module.exports = router
---
+++ src/app.js
@@ -0,0 +1,55 @@
+var http = require('http');
+var path = require('path');
+var Api = require('./api');
+var app = require('express')();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use('/api', Api);
+
+app.get('/', (req, res) => res.render('index'));
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    console.log(app.get('env'));
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+var server = app.listen(process.env.PORT || 3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('VCAH API listening at http://%s:%s', host, port);
+
+});
