5372fae683644cdcd9d147fc89878b3137571267
Cristian Caroli <cristiancaroli@gmail.com>
2015-05-11 12:29:00 +0200

    Dummy tests.

---
+++ test/main.js
@@ -0,0 +1,9 @@
+var should = require("should")
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return 2 when the value is not present', function(){
+      [1,2,3].length.should.equal(3);
+    })
+  })
+})
