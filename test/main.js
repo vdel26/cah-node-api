255a1725770728b7eef49e66dc9df22edf95ee27
Cristian Caroli <cristiancaroli@gmail.com>
2015-05-11 14:18:41 +0200

    Tests for the API

--- test/main.js
+++
@@ -1,9 +0,0 @@
-var should = require("should")
-
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return 2 when the value is not present', function(){
-      [1,2,3].length.should.equal(3);
-    })
-  })
-})
